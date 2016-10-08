44c3dc7c6c06d838d33ec800f20be380af131407
kkgarg <kamalkgarg@hotmail.com>
2016-10-08 06:10:27 +0000

    Fetch data from google

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY='AIzaSyAXC1NcqBhB7lPloqkaQrW7XinPU15jBz8';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
 
 // Create a new Component. This component should produce some html
 const App = function() {
