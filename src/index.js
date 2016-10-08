660e82234ff1ab48d8dfc6141800a89de0aa3456
kkgarg <kamalkgarg@hotmail.com>
2016-10-08 06:16:17 +0000

    converted app component from functional to class based

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY='AIzaSyAXC1NcqBhB7lPloqkaQrW7XinPU15jBz8';
+
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
   console.log(data);
 });
 
-// Create a new Component. This component should produce some html
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return(
+        <div>
+          <SearchBar />
+        </div>
+    );
+  }
 }
 
-// Take this component's generated html and put it on the page
 ReactDOM.render(<App />, document.querySelector('.container'));
 
 
