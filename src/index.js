740b11daf645ad4fbdb458ce9ddc308dc3ca0064
kkgarg <kamalkgarg@hotmail.com>
2016-10-10 00:55:52 +0000

    Video listing Changes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY='AIzaSyAXC1NcqBhB7lPloqkaQrW7XinPU15jBz8';
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      console.log(videos);
+      this.setState({videos});
+    });
+  }
+
   render() {
     return(
         <div>
           <SearchBar />
+          <VideoList videos={this.state.videos} />
         </div>
     );
   }
