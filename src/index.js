b6e4f03d8db556cfdc8c6459c4c13c858eaff670
kkgarg <kamalkgarg@hotmail.com>
2016-10-11 01:15:48 +0000

    Search implemented

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ class App extends Component {
 
   videoSearch(term) {
     YTSearch({key: API_KEY, term: term}, (videos) => {
-      console.log(videos);
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -33,7 +32,7 @@ class App extends Component {
   render() {
     return(
         <div>
-          <SearchBar />
+          <SearchBar onSearchTermChange={term=>this.videoSearch(term)} />
           <VideoDetail video={this.state.selectedVideo}/>
           <VideoList 
             onVideoSelect = {selectedVideo=>this.setState({selectedVideo})}
