93519ce7cd929bc6fc1c6b1ed9117de57ef739d5
kkgarg <kamalkgarg@hotmail.com>
2016-10-11 00:28:08 +0000

    adding video details

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY='AIzaSyAXC1NcqBhB7lPloqkaQrW7XinPU15jBz8';
-
-
 
 
 class App extends Component {
@@ -26,6 +25,7 @@ class App extends Component {
     return(
         <div>
           <SearchBar />
+          <VideoDetail video={this.state.videos[0]}/>
           <VideoList videos={this.state.videos} />
         </div>
     );
